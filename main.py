64a35143a40bb8e4e35ad23bdebd145da4aeb0bc
in1forma <95225484+in1forma@users.noreply.github.com>
2025-08-27 13:54:24 +0800

    add ACCESS_KEY

--- main.py
+++ main.py
@@ -3,3 +3,6 @@ host="www.appstankasappps.com"
 url="https://"+host
 req=requests.get(url)
 print(req.text)
+AWS_ACCESS_KEY="AKSSFFFFFF"
+AWS_SECRET_ACCESS_KEY="uB6Crsfsdeee"
+print(AWS_ACCESS_KEY,AWS_SECRET_ACCESS_KEY)
